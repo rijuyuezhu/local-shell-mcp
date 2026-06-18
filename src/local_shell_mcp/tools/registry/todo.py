16385a433c2ad36d90bf1264a07bb28187564cae
Wenrui Huang <rijuyuezhuhwr@gmail.com>
2026-06-18 13:32:19 +0800

    Return todo schemas from ops

--- src/local_shell_mcp/tools/registry/todo.py
+++ src/local_shell_mcp/tools/registry/todo.py
@@ -20,14 +20,10 @@ local_tool = TodoToolRegistry.get_tool_decorator()
 @local_tool(http_method="GET", http_path="/tools/todo")
 async def read_todos() -> ReadTodosOutput:
     """Read the current agent todo list."""
-    return ReadTodosOutput.model_validate(
-        await asyncio.to_thread(read_todos_execute)
-    )
+    return await asyncio.to_thread(read_todos_execute)
 
 
 @local_tool(http_method="POST", http_path="/tools/todo")
 async def write_todos(todos: TodosArg) -> WriteTodosOutput:
     """Replace the current structured agent todo list."""
-    return WriteTodosOutput.model_validate(
-        await asyncio.to_thread(write_todos_execute, todos)
-    )
+    return await asyncio.to_thread(write_todos_execute, todos)
